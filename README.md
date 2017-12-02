264116872bf33a36ccfa9d41c0ac223ec54dbc8d
Jatin Katyal <jatinkatyal96@gmail.com>
2017-12-02 14:10:43 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<H1>Jatin Katyal</H1>
+<h2>A small website about me, made using Node.js</h2>
---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000,()=>{
+	console.log('App started at port 3000.');
+});
